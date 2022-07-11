66e028393c588a231b3d58addae817809c434f57
Unchaptered <workstation19961002@gmail.com>
2022-07-11 21:45:40 +0900

    기본 login/join 탬플릿 추가

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title='캡슐커피 취향저격')
 
+@app.route('/join', methods=['GET'])
+def join():
+    return render_template('./pages/join.html', title='캡슐커피 취향저격')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('./pages/login.html', title='캡슐커피 취향저격')
+
 if __name__ == '__main__':
     app.run(
         '0.0.0.0',
