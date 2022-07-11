d04a1b2b3c06f27d951d827d13b0c27a48d1b5ad
Unchaptered <workstation19961002@gmail.com>
2022-07-11 23:19:40 +0900

    변수 및 함수용 주석처리

--- modules/env.py
+++ modules/env.py
@@ -1,12 +1,11 @@
-from json import load
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()                                               # .env 형식의 파일에서 변수값을 추출합니다.
 
-PORT = os.environ.get('PORT')
-MONGO_URL = os.environ.get('MONGO_URL')
-DATABASE_NAME = os.environ.get('DATABASE_NAME')
+PORT = os.environ.get('PORT')                               # 서버 실행 포트
+MONGO_URL = os.environ.get('MONGO_URL')                     # MongoDB Atlas URL 경로
+DATABASE_NAME = os.environ.get('DATABASE_NAME')             # MongoDB Atlas DB 이름
 
-COLLECTION_USER = os.environ.get('COLLECTION_USER')
-COLLECTION_CAPSULE = os.environ.get('COLLECTION_CAPSULE')
+COLLECTION_USER = os.environ.get('COLLECTION_USER')         # MongoDB Atlas Collection 이름
+COLLECTION_CAPSULE = os.environ.get('COLLECTION_CAPSULE')   # MongoDB Atlas Collection 이름
--- modules/form.py
+++ modules/form.py
@@ -1,4 +1,12 @@
 def getForm(isSuccess, message, result):
+    """
+    일정한 형태의 객체를 반환하는 순수함수입니다.
+    ```python
+        isSuccess: boolean
+        message: string
+        result: Object
+    ```
+    """
     return {
         'isSuccess': isSuccess,
         'message': message,
@@ -6,7 +14,23 @@ def getForm(isSuccess, message, result):
     };
 
 def getSuccessForm(message, result):
+    """
+    일정한 형태의 객체를 반환하는 순수함수입니다.
+    ```python
+        isSuccess: True
+        message: string
+        result: Object
+    ```
+    """
     return getForm(True, message, result)
 
 def getFailureForm(message):
+    """
+    일정한 형태의 객체를 반환하는 순수함수입니다.
+    ```python
+        isSuccess: False
+        message: string
+        result: {}
+    ```
+    """
     return getForm(True, message, {})
