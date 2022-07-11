bb65cc01747370d7b7a38cef4e7eedf90d07cd4e
Unchaptered <workstation19961002@gmail.com>
2022-07-11 18:35:08 +0900

    환경변수 파일

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +11,7 @@ def home():
     return render_template('index.html', title='캡슐커피 취향저격')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run(
+        '0.0.0.0',
+        port=os.environ.get('port'),
+        debug=True)
