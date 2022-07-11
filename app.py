37baeb340b2070041e10c77776691af81ec800dd
Unchaptered <workstation19961002@gmail.com>
2022-07-11 22:08:06 +0900

    새 환경 변수 적용

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-from env import port
+
+from env import PORT, MONGO_URL, DATABASE_NAME, COLLECTION_USER
+from database import getMongoClient
 
 app = Flask(__name__)
+database = getMongoClient(MONGO_URL)[DATABASE_NAME]
 
 @app.route('/')
 def home():
@@ -15,8 +18,31 @@ def join():
 def login():
     return render_template('./pages/login.html', title='캡슐커피 취향저격')
 
+
+@app.route('/api/join', methods=['POST'])
+def apiJoin():
+    name = request.form['name']
+    password = request.form['password']
+    
+    database[COLLECTION_USER].find_one({}) # 중복 검사
+    database[COLLECTION_USER].insert_one({}) # 아이디 생성
+
+    return jsonify({
+        'msg': 'hello'
+    })
+
+@app.route('/api/login', methods=['POST'])
+def apiLogin():
+    name = request.form['name']
+    password = request.form['password']
+
+    return jsonify({
+        'msg': 'hello'
+        # 'msg': [ name, password ]
+    })
+
 if __name__ == '__main__':
     app.run(
         '0.0.0.0',
-        port=port,
+        port=PORT,
         debug=True)
