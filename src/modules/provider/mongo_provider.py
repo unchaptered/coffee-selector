90f6a00e226114b82d25eb861ce6bfd78bfca463
Unchaptered <workstation19961002@gmail.com>
2022-07-13 18:32:21 +0900

    Refactor `mongo_provider.py`, 코드 리펙터링 진행

--- src/modules/provider/mongo_provider.py
+++ src/modules/provider/mongo_provider.py
@@ -1,7 +1,20 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, Database
+from ..config.config_provider import COLLECTION_USER, COLLECTION_CAPSULE, COLLECTION_SELECT
 
 def getMongoClient(MONGO_URL):
     """
     지정된 MONGO_URL 을 사용하여 MongoDB Atlas 서버와 연결을 하게 됩니다.
     """
     return MongoClient(MONGO_URL)
+
+def getDatabase(MONGO_URL: str, DATABASE_NAME: str) -> Database:
+    return MongoClient(MONGO_URL)[DATABASE_NAME]
+
+def getUserConnection(database: Database):
+    return database[COLLECTION_USER]
+
+def getCapsuleConnection(database: Database):
+    return database[COLLECTION_CAPSULE]
+
+def getSelectConnect(database: Database):
+    return database[COLLECTION_SELECT]
