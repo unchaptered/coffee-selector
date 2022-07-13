7feb69dfe9180ac94f79817f26415119609082dd
Unchaptered <workstation19961002@gmail.com>
2022-07-13 21:23:42 +0900

    Modify `bcrpyt.py`, 캐스팅을 포함한 해시화 함수 추가

--- src/modules/auth/bcrypt.py
+++ src/modules/auth/bcrypt.py
@@ -9,3 +9,14 @@ def getBcrypt(app: any) -> Bcrypt:
     공식문서 : https://github.com/mahenzon/flask-bcrypt
     """
     return Bcrypt(app)
+
+
+def getHashPw(brcypt: Bcrypt, pw: str):
+    """
+    getBcrypt 함수를 통해서 생성된 Brypt 모듈을 사용하여 암호화 pw 를 생성합니다.
+
+    기존의 내장 함수는 Binary 타입의 변수를 떨구기 때문에,
+
+    decode('ascii) 부분으로 문자열 타입으로 변환하였습니다.
+    """
+    return brcypt.generate_password_hash('pw').decode('ascii')
